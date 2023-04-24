8cc210bcf8753354961877289dacdc27e7a705c1
HyungwonJin <hwjin92@gmail.com>
2023-04-24 18:08:17 +0900

    refactor: refactor every thing

--- main.go
+++ main.go
@@ -1,7 +1,19 @@
 package main
 
-import "github.com/HyungwonJin/coin-practice/blockchain"
+import (
+	"fmt"
+
+	"github.com/HyungwonJin/coin-practice/blockchain"
+)
 
 func main() {
 	chain := blockchain.GetBlockchain()
+	chain.AddBlock("Second block")
+	chain.AddBlock("Third block")
+	chain.AddBlock("Fourth block")
+	for _, block := range chain.AllBlocks() {
+		fmt.Printf("Data: %s\n", block.Data)
+		fmt.Printf("Hash: %s\n", block.Hash)
+		fmt.Printf("Prev Hash: %s\n", block.PrevHash)
+	}
 }
